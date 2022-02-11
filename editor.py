965cd47fddd8d9c8f42472b11c1ca25156c7c647
Kyeu Sun Jo <42421547+Hwiet@users.noreply.github.com>
2022-02-11 11:20:32 -0500

    Video plays now

--- editor.py
+++ editor.py
@@ -1,18 +1,22 @@
 from re import X
 from PyQt5.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem
-from PyQt5.QtMultimedia import QMediaPlayer
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+# from PyQt5.QtCore import QDir, Qt, QUrl
 from pose import Joint
 from annotated_media import AnnotatedVideo, AnnotatedImage
 from typing import List
 
 
 class PoseEditor():
-    def __init__(self, media):
-        self.media = media
-        self.widget = PoseWidget(media.filename, media.poses)
-        self.widget.show()
+    def __init__(self, media) -> QWidget:
+        return NotImplementedError()
+
+
+    def get_widget(self):
+        return self.widget
 
 
     @property
@@ -36,11 +40,10 @@ class PoseEditor():
         self.new_annotation.write()
 
 
-
 class VideoPoseEditor(PoseEditor):
     def __init__(self, media: AnnotatedVideo):
-        super().__init__(media)
         self.current_frame = 0
+        self.widget = PoseWidget(QDir.current().absoluteFilePath(media.filename), media.poses)
 
 
     @property
@@ -60,7 +63,10 @@ class VideoPoseEditor(PoseEditor):
 
 
     def play(self):
-        return NotImplementedError()
+        if self.player.state() == QMediaPlayer.PlayingState:
+            self.player.pause()
+        else:
+            self.player.play()
 
 
     def pause(self):
@@ -109,11 +115,10 @@ class makeLines(QGraphicsView):
 class PoseWidget(QGraphicsView):
     def __init__(self, file, poses):
         super().__init__()
-        self.scene = QGraphicsScene()
-        self.image = self.scene.addPixmap(QPixmap(file))
         self.poses = poses
-        self.setUpScene()
-        self.setScene(self.scene)
+        self.scene = QGraphicsScene()
+        self.display = QVideoWidget()
+        self.player = QMediaPlayer()
 
     def setUpScene(self):
         point_brush = QBrush(Qt.green, Qt.SolidPattern)
@@ -121,13 +126,25 @@ class PoseWidget(QGraphicsView):
         pose = self.poses[0]
         self.points:List[JointWidget] = []
         makeLines(self.poses)
+        self.scene.addWidget(self.display)
+        self.player.setVideoOutput(self.display)
+        self.player.setMedia(QMediaContent(QUrl(file)))
+
+        self.player.play()
+
+        # add joints
+        point_size = 10
+        pose = self.poses[0]
+        self.points:List[JointWidget] = []
         for joint_name in pose.joint_cls.keys:
             point = JointWidget(*pose.joints[joint_name].position, point_size, point_size)
-            self.scene.addItem(point)
             self.points.append(point)
+            self.scene.addItem(point)
+
+        self.setScene(self.scene)
 
     def resizeEvent(self, event):
-        self.fitInView(self.image, Qt.KeepAspectRatioByExpanding)
+        self.fitInView(QRectF(self.display.rect()))
      
 
 class JointWidget(QGraphicsEllipseItem):
