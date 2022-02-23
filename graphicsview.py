316ed83ff8496af1d6788ada0f0ad19a34d67c50
Kyeu Sun Jo <42421547+Hwiet@users.noreply.github.com>
2022-02-22 22:17:57 -0500

    Remove subclass of QAbstractItemView, use QGraphicsView only

--- graphicsview.py
+++ graphicsview.py
@@ -1,10 +1,60 @@
-from PyQt5.QtWidgets import QGraphicsView
-from PyQt5.QtCore import Qt, pyqtSignal
+from mediaplayer import MediaPlayer
+
+
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QOpenGLWidget
+from PyQt5.QtMultimediaWidgets import QGraphicsVideoItem 
+from PyQt5.QtCore import *
 
 
 class GraphicsView(QGraphicsView):
     toPrevFrame = pyqtSignal()
     toNextFrame = pyqtSignal()
+    ready = pyqtSignal(QRect)
+
+    def __init__(self, video_file):
+        super().__init__()
+
+        ##
+        ## private fields
+        ##
+        self._video = QGraphicsVideoItem()
+        self._player = MediaPlayer()
+
+
+        self._player.setMedia(video_file)
+        self._player.setVideoOutput(self._video)
+        self._player.setPosition(0)
+        self._player.pause()
+        
+
+        scene = QGraphicsScene()
+        scene.addItem(self._video)
+        self.setScene(scene)
+
+        self.setAlignment(Qt.AlignCenter) 
+        self.fitInView(self._video, Qt.KeepAspectRatioByExpanding)
+
+
+        self._video.nativeSizeChanged.connect(self.changeSize)
+        self.toPrevFrame.connect(lambda: self._player.setFrame(self._player.frame() - 1))
+        self.toNextFrame.connect(lambda: self._player.setFrame(self._player.frame() + 1))
+
+   
+    @pyqtSlot()
+    def showPreviousFrame(self):
+        self._player.setFrame(self._player.frame() - 1)
+
+
+    @pyqtSlot()
+    def showNextFrame(self):
+        self._player.setFrame(self._player.frame() + 1)
+
+
+    @pyqtSlot()
+    def changeSize(self):
+        self._video.setSize(self._video.nativeSize())
+        self.fitInView(self._video, Qt.KeepAspectRatioByExpanding)
+        self.ready.emit(QRect(QPoint(), self._video.nativeSize().toSize()))
 
 
     def keyPressEvent(self, event):
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget, QGraphicsVideoItem 
 from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QAbstractScrollArea, QGraphicsWidget, QGraphicsProxyWidget
 
-from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex, QPersistentModelIndex, QVariant, QMetaType, QPointF, QSizeF
+from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex, QPersistentModelIndex, QVariant, QMetaType, QPointF, QSizeF, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QStandardItemModel
 
 
@@ -17,57 +17,35 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QDir, QUrl
 
 from pose_model import PoseModel
-from pose_view import PoseView
 from mediaplayer import MediaPlayer
 
 import pandas as pd
 import cv2
 
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QObject, QRect
+
+from graphicsview import GraphicsView
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        keyframes = pd.read_pickle('data/golfDB.pkl').loc[0]['events']
+        keyframes = [frame - keyframes[0] for frame in keyframes]
+        keyframes.pop(0)
+        keyframes.pop(len(keyframes) - 1)
+
+        media = AnnotatedVideo('data/media/Golf Swing 0.mp4', CustomFormat('data/annotation/Golf Swing 0.txt'), keyframes)
+
+        graphics = GraphicsView('data/media/Golf Swing 0.mp4')
+        self.setCentralWidget(graphics)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-
-    keyframes = pd.read_pickle('data/golfDB.pkl').loc[0]['events']
-    keyframes = [frame - keyframes[0] for frame in keyframes]
-    keyframes.pop(0)
-    keyframes.pop(len(keyframes) - 1)
-
-    media = AnnotatedVideo('data/media/Golf Swing 0.mp4', CustomFormat('data/annotation/Golf Swing 0.txt'), keyframes)
-
-    pose_model = PoseModel(media.poses)
-
-    # scene = QGraphicsScene()
-
-    # display = QGraphicsVideoItem()
-
-    # display.nativeSizeChanged.connect(lambda: display.setSize(display.nativeSize()))
-    
-
-    # player = MediaPlayer()
-    # player.setVideoOutput(display)
-    # player.setMedia('data/media/Golf Swing 0.mp4')
-
-
-    # player.setPosition(0)
-    # player.pause()
-
-    # scene.addItem(display)
-
-    pose_view = PoseView('data/media/Golf Swing 0.mp4')
-    pose_view.setModel(pose_model)
-
-    # for item in pose_view.items:
-    #     scene.addItem(item)
-
-    # view = View(scene)
-
-    # view.toPrevFrame.connect(lambda: player.setFrame(player.frame() - 1))
-    # view.toNextFrame.connect(lambda: player.setFrame(player.frame() + 1))
-
-    pose_view.show()
-
+    window = MainWindow()
+    window.showMaximized()
 
     sys.exit(app.exec_())
