fedff8ff34b7e0ac3c49b87a2cd1eef5b0877e89
Kyeu Sun Jo <42421547+Hwiet@users.noreply.github.com>
2022-03-06 13:16:54 -0500

    Fix pose entity

--- entities/pose.py
+++ entities/pose.py
@@ -1,15 +1,13 @@
-from jsonschema import validate, ValidationError
+import fastjsonschema
+from fastjsonschema import JsonSchemaException
 from typing import Any
 from PyQt5.QtGui import QStandardItem
+from functools import cached_property
 
 
 class BaseData:
-    def __init__(self, data: dict):
-        self._data = data
-
-    def data(self) -> dict:
-        """Returns data in object format"""
-        return self._data
+    def __init__(self):
+        self._data = {}
 
     def keys(self) -> set:
         return list(self._data.keys())
@@ -21,30 +19,63 @@ class BaseData:
         self._data[key] = value
 
 class Joint(BaseData, QStandardItem):
-    def __init__(self, data, name: str='name', pos: str='position'):
-        super().__init__(data)
-        self.name_key = name
-        self.pos_key = pos
+    def __init__(self, data_):
+        super().__init__()
+        try:
+            self.validate(data_)
+        except JsonSchemaException as e:
+            raise e
 
-    @property
-    def position(self) -> set:
-        return self._data[self.pos_key]
-
-    @position.setter
-    def position(self, coords: set) -> None:
-        self._data[self.pos_key] = coords
-
-    @property
-    def name(self) -> str:
-        return self._data[self.name_key]
+    @cached_property
+    def validate(self):
+        """Check that joint data contains at least a name and position
+        attribute"""
+        return fastjsonschema.compile({
+            "type": "object",
+            "properties": {
+                "name": { "type": "integer" },
+                "position": {
+                    "type": "array",
+                    "items": {
+                        "type": "number"
+                    },
+                    "minItems": 2
+                }
+            },
+            "required": [ "name", "position" ]
+        })
 
 class Pose(BaseData, QStandardItem):
-    def __init__(self, data, schema: dict):
-        super().__init__(data)
-        try:
-            validate(instance=data, schema=schema)
-        except ValidationError as e:
-            raise e
-            # del self # self-destruct
+    def isNull(self) -> bool:
+        """A pose is null if it has no joints"""
+        return "joints" in self._data
 
-        self._joints = [Joint(d) for d in data["joints"]]
+    def setJoints(self, obj):
+        """Extract joint data from pose object""" 
+        self["joints"] = [Joint(d) for d in obj['joints'] if isinstance(d, dict)]
+
+    def __init__(self, data_, validate):
+        """A wrapper for a dictionary, where the data of interest is
+        the list `self._data["joints"]`. 
+
+        Args:
+            data (object): Object representation of the pose that must
+        be validated. If data is `[]`, a null pose is created.
+            validate (function): Validation function generated with
+        `fastjsonschema.compile(<schema>)`
+
+        Raises:
+            JsonSchemaException: `data` is not valid.
+        """
+        super().__init__()
+        if isinstance(data_, list):
+            if len(data_) == 0:
+                return # create null pose
+            data_ = data_[0]
+
+        try:
+            validate(data_)
+            self.setJoints(data_)
+        except JsonSchemaException as e:
+            raise e
+            del self # self-destruct
